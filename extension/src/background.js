74adc915f9ec24dfc7ac32c5de781b9ca4c1f465
ShivanKaul <shivankaul.1993@gmail.com>
2015-03-06 13:40:13 -0500

    Now supports suggestions! (I think)

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,8 +1,45 @@
 /* 
+	function suggest
+	- fetch top 3 suggestion results using json from ChallengePost
+ */
+
+function suggest(string, suggestion) {
+
+	if(string=="") {
+		return;
+	}
+	
+	var input = encodeURIComponent(string);
+	var suggestURL =  "http://challengepost.com/software/search.json?query=" + input;
+
+	$.ajax(suggestURL, {
+		"dataType" : "json",
+		"success" : function (jsonData) {
+		
+			var resultsFromJson = jsonData.results;
+			if(resultsFromJson.length == 0) {return;} // if no suggestions
+			var suggestFromJson = []; 
+			var len = Math.min(3, resultsFromJson.length); // max 3 suggestions
+			for (i = 0; i < len; i++) {
+
+				var descripString = resultsFromJson[i].input;
+
+				suggestFromJson.push({
+					"content" : resultsFromJson[i].input, 
+					"description" : descripString
+				});
+			}
+			
+			suggestion(suggestFromJson);
+		}
+	});
+}
+
+/* 
 	function navigate
 	- navigates current tab to ChallengePost with search terms
-	- used by omnibox
  */
+
 
 function navigate(inputString) {
 	if(inputString=="") {
@@ -18,5 +55,6 @@ function navigate(inputString) {
 
 
 // Reacting to users entering "@" in the omnibox
+chrome.omnibox.onInputChanged.addListener(suggest); // support suggestions queried from ChallengePost
 chrome.omnibox.onInputEntered.addListener(navigate);
-chrome.omnibox.setDefaultSuggestion({"description" : "Search ChallengePost for %s"});
+chrome.omnibox.setDefaultSuggestion({"description" : "Search ChallengePost for "%s""});
