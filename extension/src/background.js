75c701eebd5c18914bfa61227931d5a519d1b684
ShivanKaul <shivankaul.1993@gmail.com>
2015-03-06 19:44:22 -0500

    Added suggestions, but getting 502 error if I do too much querying. Fix #1.

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,46 +1,7 @@
 /* 
-	function suggest
-	- fetch top 3 suggestion results using json from ChallengePost
- */
-
-function suggest(string, suggestion) {
-
-	if(string=="") {
-		return;
-	}
-	
-	var input = encodeURIComponent(string);
-	var suggestURL =  "http://challengepost.com/software/search.json?query=" + input;
-
-	$.ajax(suggestURL, {
-		"dataType" : "json",
-		"success" : function (jsonData) {
-		
-			var resultsFromJson = jsonData.results;
-			if(resultsFromJson.length == 0) {return;} // if no suggestions
-			var suggestFromJson = []; 
-			var len = Math.min(3, resultsFromJson.length); // max 3 suggestions
-			for (i = 0; i < len; i++) {
-
-				var descripString = resultsFromJson[i].input;
-
-				suggestFromJson.push({
-					"content" : resultsFromJson[i].input, 
-					"description" : descripString
-				});
-			}
-			
-			suggestion(suggestFromJson);
-		}
-	});
-}
-
-/* 
 	function navigate
 	- navigates current tab to ChallengePost with search terms
  */
-
-
 function navigate(inputString) {
 	if(inputString=="") {
 		// If empty, then go to ChallengePost.com
@@ -53,8 +14,58 @@ function navigate(inputString) {
 	}
 }
 
+/* 
+	function suggest
+	- suggests top 3 suggestions by querying ChallengePost
+*/
+function suggest(inputString, suggestions) {
+	// suggestions is an array of SuggestResults 
+	// Query URL: "http://challengepost.com/software/search.json?query=", thanks Robin Boutros!
+
+	if(inputString=="") {
+		return;
+	}
+
+	var inputURI = encodeURIComponent(inputString);	
+	var queryURL = "http://challengepost.com/software/search.json?query=" + inputURI;
+
+	$.ajax({
+		url: queryURL,
+		dataType: "json",
+		statusCode: {
+        	502: function () {
+        		error502 = [];
+        		error502.push({
+        			"content" : "No suggestions from ChallengePost", 
+					"description" : "No suggestions from ChallengePost"
+        		});
+            	suggestions(error502);
+        	}
+        },
+		success: function (queryResult) {
+			// get array of all software projects
+			var software = queryResult.software;
+			// if no suggestions found
+			if(software.length == 0) {return;}
+			// initialize software names
+			var softwareNames = [];
+
+			var num = Math.min(3, software.length); // a maximum of 3 suggestions
+			for (i = 0; i < num; i++) {
+				var name = software[i].name;
+
+				softwareNames.push({
+					"content" : name, 
+					"description" : name
+				});
+			}
+			suggestions(softwareNames);
+
+		}
+	});
+}
 
 // Reacting to users entering "@" in the omnibox
-chrome.omnibox.onInputChanged.addListener(suggest); // support suggestions queried from ChallengePost
+chrome.omnibox.onInputChanged.addListener(suggest);
 chrome.omnibox.onInputEntered.addListener(navigate);
 chrome.omnibox.setDefaultSuggestion({"description" : "Search ChallengePost for '%s'"});
